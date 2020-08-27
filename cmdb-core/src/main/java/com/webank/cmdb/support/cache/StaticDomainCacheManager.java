a571ba399ef609c2d0a7e7430b2b4157d8a15828
Jun Chen <junchensz@foxmail.com>
2020-08-27 10:30:57 +0800

    new cache manager for static domain

--- cmdb-core/src/main/java/com/webank/cmdb/support/cache/StaticDomainCacheManager.java
+++ cmdb-core/src/main/java/com/webank/cmdb/support/cache/StaticDomainCacheManager.java
@@ -4,6 +4,8 @@ import com.google.common.collect.ImmutableMap;
 import com.webank.cmdb.domain.AdmCiTypeAttr;
 import com.webank.cmdb.support.exception.CmdbException;
 import org.apache.commons.beanutils.BeanMap;
+import org.h2.engine.Domain;
+import org.hibernate.proxy.HibernateProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.Cache;
 import org.springframework.cache.concurrent.ConcurrentMapCache;
@@ -13,9 +15,8 @@ import org.springframework.stereotype.Component;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.lang.reflect.Field;
+import java.util.*;
 import java.util.stream.Collectors;
 
 //@Component("staticDomainCacheManager")
@@ -37,6 +38,7 @@ public class StaticDomainCacheManager<D,R extends JpaRepository<D,?>> extends Re
         private Class domainClazz = null;
         private EntityManager entityManager;
         private List<Object> domainObjs = new ArrayList<>();
+        private Map<String,Map<Object,List<Object>>> indexByProperty = new HashMap<>();
 
         public DomainCache(String name,Class domainClazz,EntityManager entityManager) {
             super(name);
@@ -48,6 +50,31 @@ public class StaticDomainCacheManager<D,R extends JpaRepository<D,?>> extends Re
             if(loaded == false){
                 loadDomainData();
             }
+        }
+
+        private void generateIndex(){
+            indexByProperty.clear();
+            Field[] fields = domainClazz.getDeclaredFields();
+            List<String> properties = Arrays.stream(fields).map(Field::getName).collect(Collectors.toList());
+            domainObjs.forEach(domain -> {
+                BeanMap domainMap = new BeanMap(domain);
+                properties.forEach(property -> {
+                    Object value = domainMap.get(property);
+                    if(value == null || value instanceof HibernateProxy)
+                        return;
+                    Map<Object,List<Object>> valueMap = indexByProperty.get(property);
+                    if(valueMap == null){
+                        valueMap = new HashMap<>();
+                        indexByProperty.put(property,valueMap);
+                    }
+                    List domainObjs = valueMap.get(value);
+                    if(domainObjs == null){
+                        domainObjs = new LinkedList();
+                        valueMap.put(value,domainObjs);
+                    }
+                    domainObjs.add(domain);
+                });
+            });
         }
 
         @Override
@@ -62,8 +89,34 @@ public class StaticDomainCacheManager<D,R extends JpaRepository<D,?>> extends Re
             }
 
             DomainCacheKey cacheKey = (DomainCacheKey)key;
-            List foundList= domainObjs.stream().filter(domain -> {return match(cacheKey,domain);}).collect(Collectors.toList());
+            //List foundList= domainObjs.stream().filter(domain -> {return match(cacheKey,domain);}).collect(Collectors.toList());
+            List foundList = fetchFromIndex(cacheKey);
             return foundList;
+        }
+
+        private List fetchFromIndex(DomainCacheKey key){
+            List resultDomainObjs = new LinkedList();
+            List<String> properties = key.getProperties();
+            for(int i=0;i< properties.size();i++){
+                String property = properties.get(i);
+                Map<Object,List<Object>> valueMap = indexByProperty.get(property);
+                if(valueMap != null){
+                    Object keyValue = key.getValues().get(i);
+                    if(keyValue == null)
+                        continue;
+                    List domainObjs = valueMap.get(keyValue);
+                    if(domainObjs == null){
+                        resultDomainObjs.clear();
+                    }else {
+                        if (resultDomainObjs.size() == 0) {
+                            resultDomainObjs.addAll(domainObjs);
+                        } else {
+                            resultDomainObjs.retainAll(domainObjs);
+                        }
+                    }
+                }
+            }
+            return  resultDomainObjs;
         }
 
         private boolean match(DomainCacheKey key,Object domainObj){
@@ -103,6 +156,7 @@ public class StaticDomainCacheManager<D,R extends JpaRepository<D,?>> extends Re
             if(result != null && result.size()>0){
                 domainObjs = result;
             }
+            generateIndex();
             loaded = true;
         }
     }
